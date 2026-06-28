f9f0e71a737aa7406ae04cf63bb173ca7a322022
Pierre Tinard <ptinard11@gmail.com>
2026-06-28 10:15:23 +0200

    feat(news): add crm-immo-nest news card, fix slide-in order and delays

--- frontend/components/sections/news/NewsSection.vue
+++ frontend/components/sections/news/NewsSection.vue
@@ -4,26 +4,32 @@
     <div class="grid grid-cols-1 md:grid-cols-3 gap-8 px-6 mb-20">
 
       <NewsCard
-      v-slide-in
-      :title="$t('News1Title')"
-      :text="$t('News1Text')"
-      :date="$t('News1Date')"
+        v-slide-in="0"
+        :title="$t('News4Title')"
+        :text="$t('News4Text')"
+        :date="$t('News4Date')"
       />
 
       <NewsCard
-        v-slide-in="150"
+        v-slide-in="250"
+        :title="$t('News3Title')"
+        :text="$t('News3Text')"
+        :date="$t('News3Date')"
+      />
+
+       <NewsCard
+        v-slide-in="350"
         :title="$t('News2Title')"
         :text="$t('News2Text')"
         :date="$t('News2Date')"
       />
 
       <NewsCard
-        v-slide-in="300"
-        :title="$t('News3Title')"
-        :text="$t('News3Text')"
-        :date="$t('News3Date')"
+      v-slide-in="450"
+      :title="$t('News1Title')"
+      :text="$t('News1Text')"
+      :date="$t('News1Date')"
       />
-
     </div>
 
   </section>
